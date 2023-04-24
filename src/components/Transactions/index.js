dc79435118d653bf04450b3c8324e92b61a5c008
Ajith shetty <ajith.shettyy@gmail.com>
2023-04-24 20:17:52 +0530

    add transaction id

--- src/components/Transactions/index.js
+++ src/components/Transactions/index.js
@@ -2,21 +2,25 @@ function Transactions() {
   return (
     <table>
       <tr>
+        <th>Transaction ID</th>
         <th>Amount</th>
         <th>Time</th>
         <th>Status</th>
       </tr>
       <tr>
+        <td>1234</td>
         <td>100</td>
         <td>224/04/2023 10:34AM</td>
         <td>Success</td>
       </tr>
       <tr>
+        <td>456</td>
         <td>100</td>
         <td>224/04/2023 10:34AM</td>
         <td>Success</td>
       </tr>
       <tr>
+        <td>7898</td>
         <td>100</td>
         <td>224/04/2023 10:34AM</td>
         <td>Failed</td>
